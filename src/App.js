7306d908449dcdb9a8a09e41d2c3109e2b8816e2
Vitor <vitor.pereira@webjump.com.br>
2022-04-13 22:30:01 -0300

    adding routes to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./components/Products";
 
 function App() {
-  return <div className="App">ALO</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/Home" />
+          <Route element={<Products />} path="/Products" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div>Essa aqui é a HOME!</div>
+    </>
+  );
+};
+
+export default Home;
